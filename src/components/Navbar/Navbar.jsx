426ee98eb08cde8a2d8cf14a539d4592c7b15f06
jakariamasum <jmphy698@gmail.com>
2023-10-04 16:13:35 +0600

    navbar responsive issue fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,24 @@
 import styles from './Navbar.module.css';
 import bar from '../../assets/bars.png'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-scroll';
 const Navbar = () => {
-    const mobile = window.innerWidth <= 768 ? true : false;
     const [open, setOpen] = useState(false);
+    useEffect(() => {
+        function handleResize() {
+            const isMobile = window.innerWidth <= 768;
+            setOpen(false);
+            setMobile(isMobile);
+        }
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const [mobile, setMobile] = useState(window.innerWidth <= 768);
     return (
         <div className={styles.header}>
             <h2>Smart and Fit</h2>
@@ -15,20 +29,20 @@ const Navbar = () => {
                     </div>
                 ) : (
                     <ul className={styles.navmenu}>
-                        <li onClick={()=>setOpen(false)}>
-                            <Link to='home' spy={true} smooth={true} onClick={()=>setOpen(false)}>Home</Link>
+                        <li onClick={() => setOpen(false)}>
+                            <Link to='home' spy={true} smooth={true} onClick={() => setOpen(false)}>Home</Link>
                         </li>
-                        <li onClick={()=>setOpen(false)}>
-                        <Link to='programms' spy={true} smooth={true} onClick={()=>setOpen(false)}>Programms</Link>
+                        <li onClick={() => setOpen(false)}>
+                            <Link to='programms' spy={true} smooth={true} onClick={() => setOpen(false)}>Programms</Link>
                         </li>
-                        <li onClick={()=>setOpen(false)}>
-                        <Link to='reasons' spy={true} smooth={true} onClick={()=>setOpen(false)}>Why Us</Link>
+                        <li onClick={() => setOpen(false)}>
+                            <Link to='reasons' spy={true} smooth={true} onClick={() => setOpen(false)}>Why Us</Link>
                         </li>
-                        <li onClick={()=>setOpen(false)}>
-                        <Link to='plans' spy={true} smooth={true} onClick={()=>setOpen(false)}>Plans</Link>
+                        <li onClick={() => setOpen(false)}>
+                            <Link to='plans' spy={true} smooth={true} onClick={() => setOpen(false)}>Plans</Link>
                         </li>
-                        <li onClick={()=>setOpen(false)}>
-                        <Link to='testimonials' spy={true} smooth={true} onClick={()=>setOpen(false)}>Testimonials</Link>
+                        <li onClick={() => setOpen(false)}>
+                            <Link to='testimonials' spy={true} smooth={true} onClick={() => setOpen(false)}>Testimonials</Link>
                         </li>
                     </ul>
                 )
