9b2771b117ed3bcf25600f2ba55417acd466fb00
jakariamasum <jmphy698@gmail.com>
2023-10-04 15:48:59 +0600

    responsive ready

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,9 +12,6 @@ const Footer = () => {
                     <img src={linkedin} alt="" />
                     <img src={instagram} alt="" />
                 </div>
-                <div className={Styles.logo}>
-                    <span>Smart and Fit</span>
-                </div>
             </div>
             <div className={`make-blur ${Styles.leftBlur}`}></div>
             <div className={`make-blur ${Styles.rightBlur}`}></div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,27 @@
 import styles from './Navbar.module.css';
+import bar from '../../assets/bars.png'
+import { useState } from 'react';
 const Navbar = () => {
+    const mobile = window.innerWidth <= 768 ? true : false;
+    const [open, setOpen] = useState(false);
     return (
         <div className={styles.header}>
             <h2>Smart and Fit</h2>
-            <ul className={styles.navmenu}>
-                <li>Home</li>
-                <li>Programms</li>
-                <li>Why Us</li>
-                <li>Plans</li>
-                <li>Testimonials</li>
-            </ul>
+            {
+                open === false && mobile === true ? (
+                    <div style={{ backgroundColor: 'var(--appColor)', borderRadius: '5px', padding: '0.5rem' }}>
+                        <img src={bar} alt="" style={{ width: '1.5rem', height: '1.5rem' }} onClick={() => setOpen(true)} />
+                    </div>
+                ) : (
+                    <ul className={styles.navmenu}>
+                        <li onClick={()=>setOpen(false)}>Home</li>
+                        <li onClick={()=>setOpen(false)}>Programms</li>
+                        <li onClick={()=>setOpen(false)}>Why Us</li>
+                        <li onClick={()=>setOpen(false)}>Plans</li>
+                        <li onClick={()=>setOpen(false)}>Testimonials</li>
+                    </ul>
+                )
+            }
         </div>
     );
 };
